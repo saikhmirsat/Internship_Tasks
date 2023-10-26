5e25491fa217c53b72c207a9e475cffe260dc9a9
saikhmirsat <saikh.mirsat.786@gmail.com>
2023-10-26 18:38:30 +0530

    add

--- Web_bucket_task/Backend/Config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-require('dotenv').config();
-
-const connection = mongoose.connect('mongodb+srv://numetry:numetry@cluster0.2ba9eik.mongodb.net/Products?retryWrites=true&w=majority')
-
-module.exports = {
-    connection
-}
