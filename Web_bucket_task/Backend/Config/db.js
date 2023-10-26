f2a35bc60d9e78e69cb223d33e7e080881c7ce47
saikhmirsat <saikh.mirsat.786@gmail.com>
2023-10-26 18:31:50 +0530

    add

--- Web_bucket_task/Backend/Config/db.js
+++ Web_bucket_task/Backend/Config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 require('dotenv').config();
 
-const connection = mongoose.connect(process.env.URL)
+const connection = mongoose.connect('mongodb+srv://numetry:numetry@cluster0.2ba9eik.mongodb.net/Products?retryWrites=true&w=majority')
 
 module.exports = {
     connection
--- Web_bucket_task/Backend/index.js
+++ Web_bucket_task/Backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 app.use("/api/products", productRoutes);
 
-const port = process.env.PORT
+const port = 8000
 app.listen(port, async () => {
     console.log(`Server is running on port ${port}`);
     try {
